3d545716d3ebc2e3a64986e2af185ad41a25c822
Skerfold <lucas.p.souza11@aluno.senai.br>
2025-09-11 11:25:14 -0300

    11:25 Mobile

--- Firebase/to_do_list_firebase/lib/main.dart
+++ Firebase/to_do_list_firebase/lib/main.dart
@@ -3,12 +3,14 @@ import 'package:flutter/material.dart';
 import 'package:to_do_list_firebase/firebase_options.dart';
 import 'package:to_do_list_firebase/views/autenticacao_view.dart';
 
-void main() async{ 
+void main() async{
   WidgetsFlutterBinding.ensureInitialized(); //garantir a inicialização dos bindings
   //inicializa o firebase -  ao mesmo tempo que abre as telas
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform
-  );
+  if (Firebase.apps.isEmpty) {
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform
+    );
+  }
   runApp(MaterialApp(
     title: "Lista de Tarefas",
     home: AutenticacaoView(), //direciona para tela de autenticação
